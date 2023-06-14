d9dc285367072dd378bb73e7a39b6fdf82d9fcae
Kang <rkdalsgur032@unist.ac.kr>
2023-06-15 02:21:55 +0900

    fix api route

--- core/api/getStockData.ts
+++ core/api/getStockData.ts
@@ -1,5 +1,5 @@
 const getStockData = async () => {
-  const response = await fetch("http://localhost:3000/api/data");
+  const response = await fetch("/api/data");
   const data = await response.json();
   return data;
 };

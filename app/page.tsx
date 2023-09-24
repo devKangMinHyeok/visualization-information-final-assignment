ef66e2f669efbfc77a4600c5a7b48c651b9821f6
Kang <rkdalsgur032@unist.ac.kr>
2023-09-24 17:07:46 +0900

    user-pilot

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,19 @@ import DateSliderContainer from "@/layouts/DateSliderContainer";
 import LineChartContainer from "@/layouts/LineChartContainer";
 import ProfitRankChartContainer from "@/layouts/ProfitRankChartContainer";
 import RadarChartContainer from "@/layouts/RadarChartContainer";
+import { useEffect } from "react";
+import { Userpilot } from "userpilot";
 
 export default function Home() {
+  useEffect(() => {
+    Userpilot.initialize("NX-be3f6990");
+    Userpilot.identify("1", {
+      name: "강민혁",
+      email: "rkdalsgur032@gmail.com",
+      created_at: "2021-06-01T00:00:00.000Z",
+    });
+  }, []);
+
   return (
     <main>
       <div className="w-screen h-screen flex flex-col">
